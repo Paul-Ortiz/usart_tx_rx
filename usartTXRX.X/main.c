89c961e9cc224b0bff0f4caee908eaedcdadf37e
Paul-Ortiz <paul_ortiz02@hotmail.com>
2024-01-19 20:26:22 -0500

    Update main
    Add configurations parameters

--- usartTXRX.X/main.c
+++ usartTXRX.X/main.c
@@ -6,9 +6,17 @@
  */
 
 #include <xc.h>
-#pragma config FOSC = HS    //4Mhz
+#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF       // Power-up Timer Enable bit (PWRT enabled)
+#pragma config BOREN = ON       // Brown-out Reset Enable bit (BOR enabled)
+#pragma config LVP = OFF        // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
 
-#define _XTAL_FREQ 20000000  //4Mhz
+
+#define _XTAL_FREQ 20000000  //20Mhz
 
 #define led PORTCbits.RC4
 
